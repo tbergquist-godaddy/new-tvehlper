6ed541896324228e7fff0b38fa43669539069a73
Trond Bergquist <tbergquist@godaddy.com>
2024-02-19 22:45:21 +0100

    connect to db and generate token

--- app/login/components/login-form.tsx
+++ app/login/components/login-form.tsx
@@ -12,27 +12,42 @@ export default function LoginForm() {
   // TODO: Formik
   const [usernameError, setUserNameError] = useState<boolean | string>(false);
   const [passwordError, setPasswordError] = useState<boolean | string>(false);
+  const [loginError, setLoginError] = useState('');
+  const [loading, setLoading] = useState(false);
   return (
-    <form
-      action={async (formData) => {
-        const val = await login(formData);
-        const containsField = (field: string) => {
-          for (const error of val) {
-            if (error.path.includes(field)) {
-              return true;
+    <>
+      <form
+        action={async (formData) => {
+          setUserNameError(false);
+          setPasswordError(false);
+          setLoginError('');
+          setLoading(true);
+          const val = await login(formData);
+          const containsField = (field: string) => {
+            for (const error of val) {
+              if (typeof error !== 'string' && error.path.includes(field)) {
+                return true;
+              }
             }
+            return false;
+          };
+          setUserNameError(containsField('username') && 'This field is required');
+          setPasswordError(containsField('password') && 'This field is required');
+          if (typeof val === 'string') {
+            setLoginError('Incorrect username or password');
           }
-          return false;
-        };
-        setUserNameError(containsField('username') && 'This field is required');
-        setPasswordError(containsField('password') && 'This field is required');
-      }}
-    >
-      <Section>
-        <TextInput name="username" label="Username" error={usernameError} />
-        <TextInput name="password" label="Password" type="password" error={passwordError} />
-        <Button type="submit">Login</Button>
-      </Section>
-    </form>
+          setLoading(false);
+        }}
+      >
+        <Section>
+          <TextInput name="username" label="Username" error={usernameError} />
+          <TextInput name="password" label="Password" type="password" error={passwordError} />
+          {loginError != null && <p className="text-red-600">{loginError}</p>}
+          <Button disabled={loading} type="submit">
+            {loading ? 'loading...' : 'Login'}
+          </Button>
+        </Section>
+      </form>
+    </>
   );
 }
