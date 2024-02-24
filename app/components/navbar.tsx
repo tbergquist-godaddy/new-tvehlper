4f336bd2b04587df5f9d69e6c471738cf1981af5
Trond Bergquist <tbergquist@godaddy.com>
2024-02-25 00:16:11 +0100

    add favorites link in navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,16 +7,24 @@ import { getLoggedInUserId } from '@/src/services/get-id-from-cookie';
 
 export default async function Navbar() {
   const userId = await getLoggedInUserId();
+  const isLoggedIn = userId != null;
   return (
     <nav className="bg-blue-600 text-white">
       <Container className="flex justify-between items-center">
-        <div className="text-2xl py-3">
-          <Link className="py-3" href="/">
-            Tvhelper
-          </Link>
+        <div className="flex items-center gap-3">
+          <div className="text-2xl py-3">
+            <Link className="py-3" href="/">
+              Tvhelper
+            </Link>
+          </div>
+          {isLoggedIn && (
+            <Link className="py-3" href="/favorites">
+              Favorites
+            </Link>
+          )}
         </div>
         <div>
-          {userId == null ? (
+          {!isLoggedIn ? (
             <Link className="py-3" href="/login">
               Login
             </Link>
